9e4c9e5da8057b3cabeb44453230a0700273bc1e
NavardoW <navardowilliamsrei@gmail.com>
2025-12-31 11:21:00 -0500

    base completed

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,1 +1,8 @@
 export { githubIcon } from './github';
+export { linkedinIcon } from './linkedin';
+export { twitterIcon } from './twitter';
+export { instagramIcon } from './instagram';
+export { folderIcon } from './folder';
+export { contactIcon } from './contact';
+export { blogIcon } from './blog';
+export { aboutIcon } from './about';
