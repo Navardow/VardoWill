09101e77504f4cd08ff6960c297a2e34451825bc
NavardoW <navardowilliamsrei@gmail.com>
2026-02-02 17:49:21 -0500

    feat: database integration and refactor

--- db/config.ts
+++ db/config.ts
@@ -2,20 +2,34 @@ import { defineDb, defineTable, column } from 'astro:db';
 
 const Projects = defineTable({
     columns: {
-        id: column.number({primaryKey: true}),
-        title: column.text(),
-        summary: column.text(),
-        documentation: column.json(),
-        tags: column.json(),
-        image: column.text(),
-        featured: column.boolean({optional: true}),
+        id: column.number({ primaryKey: true }),
+        title: column.text({ optional: false }),
+        summary: column.text({ optional: false }),
+        documentation: column.text({ optional: false }),
+        tags: column.json({ optional: true }),
+        image: column.text({ optional: false }),
+        featured: column.boolean({ optional: true, default: false }),
+        date: column.date({ optional: true }),
+    },
+})
+
+const Blogs = defineTable({
+    columns: {
+        id: column.number({ primaryKey: true }),
+        title: column.text({ optional: false }),
+        summary: column.text({ optional: false }),
+        image: column.text({optional: false }),
+        postContent: column.text( { optional: false } ),
+        tags: column.json({ optional: true }),
+        date: column.date({ optional: true }),
     },
 })
 
 // https://astro.build/db/config
 export default defineDb({
     tables: {
-        Projects
+        Projects,
+        Blogs,
     }
 });
 
